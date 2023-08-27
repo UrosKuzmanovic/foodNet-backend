cb8b698cddefd9aaa4cd31c957f3e0144fcc6d53
Uros Kuzmanovic <uros.kuzmanovic@ewasoft.rs>
2023-08-27 19:55:25 +0200

    Get analytics by time

--- src/Controller/AnalyticsController.php
+++ src/Controller/AnalyticsController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Dto\AnalyticsDto;
 use App\EventListener\Events\SendAnalyticsEvent;
+use App\Service\Microservices\AnalyticsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
@@ -17,22 +19,25 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 class AnalyticsController extends AbstractController
 {
 
+    private AnalyticsService $analyticsService;
     private SerializerInterface $serializer;
     private EventDispatcherInterface $dispatcher;
 
     public function __construct(
+        AnalyticsService $analyticsService,
         SerializerInterface $serializer,
         EventDispatcherInterface $dispatcher
     )
     {
+        $this->analyticsService = $analyticsService;
         $this->serializer = $serializer;
         $this->dispatcher = $dispatcher;
     }
 
     /**
-     * @Route("/send", name="app_analytics")
+     * @Route("/send", name="send_analytics")
      */
-    public function index(Request $request): JsonResponse
+    public function send(Request $request): JsonResponse
     {
         /** @var AnalyticsDto $analytics */
         $analytics = $this->serializer->deserialize($request->getContent(), AnalyticsDto::class, 'json');
@@ -40,8 +45,25 @@ class AnalyticsController extends AbstractController
         $this->dispatcher->dispatch(new SendAnalyticsEvent($analytics), SendAnalyticsEvent::class);
 
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AnalyticsController.php',
+            'status' => Response::HTTP_OK,
+            'message' => 'Analytics sent',
+        ]);
+    }
+
+    /**
+     * @Route("/get-time", name="get_time_analytics")
+     */
+    public function getTime(Request $request): JsonResponse
+    {
+        /** @var AnalyticsDto $analytics */
+        $analytics = $this->serializer->deserialize($request->getContent(), AnalyticsDto::class, 'json');
+
+        $analyticsHttpDto = $this->analyticsService->post($analytics, 'test/get-time');
+
+        return $this->json([
+            'status' => $analyticsHttpDto->getStatus(),
+            'message' => $analyticsHttpDto->getMessage(),
+            'data' => $analyticsHttpDto->getData(),
         ]);
     }
 }
--- src/Entity/Dto/AnalyticsHttpDto.php
+++ src/Entity/Dto/AnalyticsHttpDto.php
@@ -4,4 +4,29 @@ namespace App\Entity\Dto;
 
 class AnalyticsHttpDto extends MicroserviceHttpDto
 {
+
+    private array $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     * @return AnalyticsHttpDto
+     */
+    public function setData(array $data): AnalyticsHttpDto
+    {
+        $this->data = $data;
+        return $this;
+    }
 }
